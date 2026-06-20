9e58f0456d98834626ae86b3df7feb123ede515c
dals1nnn <eigabriel139@gmail.com>
2026-06-20 19:13:16 -0300

    added get, delete, patch

--- app.js
+++ app.js
@@ -16,14 +16,24 @@ function getCurrentTime(){
     return currentTime
 }
 
+
+
+
+
+///MAIN
 const method = process.argv[2]
 const second = process.argv[3]
+const third = process.argv[4]
+
+
+
 ///post put delete get
-if (method !== "post" && method !== "delete" && method !== "get"){
-    console.log(`Error, command ${method} is not valid. try: post, delete, get`)
+if (method !== "post" && method !== "delete" && method !== "get" && method !== "patch"){
+    console.log(`Error, command ${method} is not valid. try: post, delete, get, patch`)
     process.exit(0)
 }
 
+///post method
 if (method === "post"){
     if (second === undefined){
         console.log("Type the task that you want to add after the POST")
@@ -52,3 +62,58 @@ if (method === "post"){
     }
 }
 
+
+
+
+
+
+///add update and get
+if (method == "get"){
+    if (second === undefined){
+        const rawData = await readFile(".\\tasks.json", "utf8")
+        const jsonData = JSON.parse(rawData)
+        console.log(jsonData)
+    }else{
+         //add search by id
+        const rawData = await readFile(".\\tasks.json", "utf8")
+        const jsonData = JSON.parse(rawData)
+        console.log(jsonData[(parseInt(second) - 1)])
+    }
+
+}
+
+
+
+
+
+
+
+if (method == "delete"){
+    if (second == undefined){
+        console.log("After the delete, please inform the ID of the task that you want to exclude")
+        process.exit(0)
+    }else{
+        const rawData = await readFile(".\\tasks.json", "utf-8")
+        const jsonData = JSON.parse(rawData)
+        jsonData.splice((parseInt(second) - 1), 1)
+        await writeFile(".\\tasks.json", JSON.stringify(jsonData, null, 2), "utf-8")
+        console.log(jsonData)
+    }
+}
+
+
+if (method === "patch"){
+    if (second === undefined || typeof parseInt(second) !== 'number'){
+        console.log("Please enter the ID of the task that you want to update and what is the status")
+        console.log("Example: node app.js patch 1 ´in progress´")
+        process.exit(0)
+    }
+    let id = (parseInt(second) - 1)
+
+    const rawData = await readFile(".\\tasks.json", "utf-8")
+    const jsonData = JSON.parse(rawData)
+    jsonData[id]["status"] = third
+    jsonData[id]["updatedAt"] = getCurrentTime()
+    await writeFile(".\\tasks.json", JSON.stringify(jsonData, null, 2), "utf-8")
+    console.log(jsonData)
+}
